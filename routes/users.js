415673d2db40a0588ddacacc93a15cf766719d36
YassineK69 <kninech.yassine@hotmail.fr>
2025-07-17 12:57:07 +0200

    creation sign up route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../models/connection');
+const User = require('../models/users'); // Model User à définir
+const {checkBody } = require('../modules/checkBody')
+
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+
+/* signUp */
+router.post('/users', (req, res,) => {
+  if(!checkBody(req.body, ['username', 'password'])) {
+     res.json({result: false, error: 'Missing or empty fields'});
+  
+     return;  
+  };
+ 
+// check if the user has not already been register
+User.findOne({ username: req.body.username}).then(data => {
+  if(data === null){
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    const newUser = new User ({
+      username: req.body.username,
+      password: hash,
+      token: uid2(32),
+    });
+
+    newUser.save().then((newDoc) => {
+      res.json({result: true, token: newDoc.token});
+    });
+  }else{
+    // User already exist in database
+    res.json ({ result: false, error: 'User already exists'})
+  }
+});
 });
 
+
 module.exports = router;
+
