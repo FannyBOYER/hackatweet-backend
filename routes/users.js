ca27ed616140c928ce1c24bfc728ac81c07e5c46
Dal ferro Christian <christiandalferro@gmail.com>
2025-07-17 14:25:47 +0200

    signin route réalisée avec Yassine

--- routes/users.js
+++ routes/users.js
@@ -16,26 +16,42 @@ router.post('/signup', (req, res,) => {
      return;  
   };
  
-// check if the user has not already been register
-User.findOne({ username: req.body.username}).then(data => {
-  if(data === null){
-    const hash = bcrypt.hashSync(req.body.password, 10);
-    const newUser = new User ({
-      name: req.body.name,
-      username: req.body.username,
-      password: hash,
-      token: uid2(32),
-    });
+  // check if the user has not already been register
+  User.findOne({ username: req.body.username}).then(data => {
+    if(data === null){
+      const hash = bcrypt.hashSync(req.body.password, 10);
+      const newUser = new User ({
+        name: req.body.name,
+        username: req.body.username,
+        password: hash,
+        token: uid2(32),
+      });
 
-    newUser.save().then((newDoc) => {
-      res.json({result: true, token: newDoc.token});
-    });
-  }else{
-    // User already exist in database
-    res.json ({ result: false, error: 'User already exists'})
+      newUser.save().then((newDoc) => {
+        res.json({result: true, token: newDoc.token});
+      });
+    }else{
+      // User already exist in database
+      res.json ({ result: false, error: 'User already exists'})
+    }
+  });
+});
+
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
   }
+
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'User not found or wrong password' });
+    }
+  });
 });
-});
+
 
 
 module.exports = router;
